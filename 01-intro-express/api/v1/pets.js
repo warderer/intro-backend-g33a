ee726f7eb576166a376233b33e51caa7946bae4c
César Guerra <warderer@gmail.com>
2024-09-19 21:39:57 -0600

    ✨ Query en express - con pets

--- 01-intro-express/api/v1/pets.js
+++ 01-intro-express/api/v1/pets.js
@@ -24,9 +24,9 @@ const petList = {
     ]
 }
 
-router.get('/api/v1/pets', (req, res) => {
-    res.json(petList)
-})
+// router.get('/api/v1/pets', (req, res) => {
+//     res.json(petList)
+// })
 
 /* PARAMS */
 // Un Param es un valor que se envía en la URL de una petición HTTP
@@ -44,5 +44,23 @@ router.get('/api/v1/pets/:petId', (req, res) => {
     }
 })
 
+/* QUERY */
+// Son similares a los PARAMS, pero se suelen usar para filtrar o buscar información. Sobre todo cuando ocupamos mandar más de un dato.
+// Las QUERY son abiertas, no definidmos los nombres de antemano. La responsabilidad del BACKEND es SOLO tomar en cuenta las queries que le interesen.
+// Ejemplo: /api/v1/pets?age=3&type=dog
+router.get('/api/v1/pets', (req, res) => {
+    // El objeto que contiene las query es: req.query. Es un objeto que contiene todas las queries que se mandaron en la petición por el usuario.
+    console.log(req.query)
+
+    const { age, type } = req.query
+
+    const filteredPets = petList.pets.filter(pet =>
+        type && age ? pet.type === type && pet.age === parseInt(age) :
+        type ? pet.type === type :
+        age ? pet.age === parseInt(age) : true
+    )
+
+    res.json(filteredPets)
+})
 
 module.exports = router
