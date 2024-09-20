3afab7eb5fb37f345439d044b06e30de1ce83000
César Guerra <warderer@gmail.com>
2024-09-19 20:44:42 -0600

    ✨ Puerto dinamico con express

--- 01-intro-express/index.js
+++ 01-intro-express/index.js
@@ -1,6 +1,9 @@
 // #1 Llamar a la biblioteca de express (importarla)
 const express = require('express')
 
+// Hago una variable para el puerto, si no hay un puerto definido en las variables de entorno, entonces el puerto será el 3000
+const PORT = process.env.PORT || 3000
+
 // #2a Crear una instancia de express
 const app = express()
 
@@ -13,6 +16,6 @@ app.get('/', function (req, res) {
 })
 
 // #4 Levantar el servidor, para comenzar a escuchar peticiones en un puerto específico (en este caso el 3000)
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000 🚀')
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT} 🚀`)
 })
